e196b5a0f14f5eaef3b5671b15c98c0307840796
ntabucejo <ntabucejo@gmail.com>
2022-06-22 22:19:08 +0800

    Update defaults

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiHandler } from "next";
 
-type Data = {
-  name: string
-}
+const handler: NextApiHandler = (request, response) => {
+  response.status(200).json({ name: "Nikko Abucejo" });
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default handler;
